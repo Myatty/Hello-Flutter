9c13995fa6b2ee661ec999ef6e38266d5e282e1f
Myatty <myintmyatps664@gmail.com>
2024-12-19 15:39:37 +0630

    Add column and children

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,16 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Container(
+          padding: EdgeInsets.all(20),
           height: double.infinity,
           width: double.infinity,
           decoration: BoxDecoration(
             color: Colors.blue[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [Text("Text 1"), Text("Text 2"), Text("Text 3")],
           ),
         ),
       ),
